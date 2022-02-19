bdb8e33bc1ea56d4c2fb950b142a16ce86eef71b
matemaster9 <68363002+matemaster9@users.noreply.github.com>
2022-02-19 15:03:58 +0800

    20220219 #comment SpringWeb环境搭建

---
+++ WebServer/src/test/java/cs/matemaster/WebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.matemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
