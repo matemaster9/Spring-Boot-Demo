f5949d091cafa3a0f3196fea8f2d45a650ae2261
matemaster <matemaster@163.com>
2022-02-24 15:20:06 +0800

    20220224 #comment spring-rabbit基础环境

---
+++ spring-rabbit/src/main/java/cs/matemaster/SpringRabbitBoot.java
@@ -0,0 +1,16 @@
+package cs.matemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: MateMaster
+ * @Date: 2022/2/24 15:17
+ */
+@SpringBootApplication
+public class SpringRabbitBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitBoot.class, args);
+    }
+}
